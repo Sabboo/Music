bcc9e858cceda0756a9ec612a1eaa19afb462337
Ahmed Saber <sander.sader@gmail.com>
2019-08-06 17:27:15 +0200

    Moving MainActivity to presentation package.

--- app/src/main/java/com/example/music/presentation/MainActivity.java
+++ app/src/main/java/com/example/music/presentation/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.music;
+package com.example.music.presentation;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.music.R;
 
 public class MainActivity extends AppCompatActivity {
 
