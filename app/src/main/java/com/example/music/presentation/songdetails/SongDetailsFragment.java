69de7d01da69dc7c612301e93b5888a911c77268
Ahmed Saber <sander.sader@gmail.com>
2019-08-06 23:56:31 +0200

    Feature song details implementation; Fragment, Layout

---
+++ app/src/main/java/com/example/music/presentation/songdetails/SongDetailsFragment.java
@@ -0,0 +1,26 @@
+package com.example.music.presentation.songdetails;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.music.R;
+
+public class SongDetailsFragment extends Fragment {
+
+    public SongDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_song_details, container, false);
+    }
+
+}
